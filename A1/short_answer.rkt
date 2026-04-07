82334c39e866b2de31a60a6b1c384d50d11eef40
Kevin Lu <kevinlu3234@gmail.com>
2026-04-06 21:28:11 -0700

    done

---
+++ A1/short_answer.rkt
@@ -0,0 +1,37 @@
+#lang scribble/manual
+@title{Short Answer A1}
+
+Beating the Averages
+Question 1
+	The three features of the Lisp language that Graham believes gave his company a completive 
+    advantage is a macro, Lisp macros; that allow code to be unique allowing to build complex 
+    abstract. Basically, it allows programs that provide an advantage in building complex 
+    abstraction and able to solve problems better than other languages. The second feature 
+    is the garbage collection that is introduced by Lisp and runtime typing which help remove 
+    the need for programmer to write repetitive code and construct less resources logic. 
+    These features boost the rapid development in the market and enable them to release 
+    software quicker and be able to work with bigger teams. The third features are their so 
+    called "secret weapon" that they wrote in a weird AI language with bizarre syntax full of 
+    parentheses, which lead most business to avoid it and use the standard tool. 
+    Because most competitors or businesses viewed Lisp as more for academic rather than a tool 
+    for production, they didn't pay much attention to the language itself which was the source 
+    of Viaweb performance.
+
+Question 2
+	The two observations that I found most significant in Graham's article is how he talked 
+    about the Blub Paradox explaining why many programmers fail to realize they are using 
+    a less powerful tool and that "Blub programmer is looking down the power continuum." 
+    It is easier to look down on lower language than looking at a higher language and think 
+    they don't see the power/use in them. The other observation is when he stated "average 
+    performance means that you'll go out of business…" I think that what he means is most 
+    average startup fail due to not trying hard enough. Most startups play it safe by using 
+    same language, marketing, etc. would lead to an average result in which big companies 
+    are likely to achieve it better.  Basically, to survive is to become the odd one to succeed
+     in a startup and to do better than everyone else.
+
+Java's Cover
+Question 3
+	- When the language is designed by the creator, is it just for them to use or as a product
+is it for others to use as well?
+	- Are new languages required a lot of marketing to be successful like python to boost popularity?
+	- Do people love the language they program in or are they force to use it?
