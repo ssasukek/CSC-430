0e57f987e53077860e3d714cf975d45f6abf3406
Kevin Lu <kevinlu3234@gmail.com>
2026-04-09 21:30:08 -0700

    a2 skele

---
+++ A2/asgn2.rkt
@@ -0,0 +1,141 @@
+#lang typed/racket
+(require typed/rackunit)
+
+;; Test case usage: (check-equal? ...) or (check-= ...)
+
+
+;; 2.1 Textbook Problems
+
+;; Problem 2.3.3
+    ; An old-style movie theater has a simple profit function. 
+    ; Each customer pays $5 per ticket. Every performance costs 
+    ; the theater $20, plus $.50 per attendee. Develop the function 
+    ; total-profit. It consumes the number of attendees (of a show) 
+    ;and produces how much income the attendees produce. 
+
+
+
+
+
+
+
+
+;; Problem 3.3.3
+    ; Develop area-cylinder. The program consumes the radius of the cylinder's 
+    ; base disk and its height. Its result is the surface area of the cylinder.
+
+
+
+
+
+
+
+
+
+
+;; 2.2 Playing Card Manufacturing 
+    ; Develop the function next-card
+    ; Accepts a card and return the next higher card that appear in same suit straight 
+
+
+
+
+
+
+
+
+
+
+;; 2.3 Low-degree Polynomial
+    ; Develop a data defintion for a polynomial
+    ; vairents call Linear and Quadratic
+
+
+
+
+
+
+
+
+
+
+
+;; 2.4 Derivative
+    ; Develop the fucntion derivative
+    ; That accepts a polynomial and returns another polynomial representing its derivative
+    ; do not return a quadratic polynomial whose first coefficient is zero
+
+
+
+
+
+
+
+
+
+
+;; 2.5 Binary tree
+    ; Develop a data definition for a full binary tree called BTree
+    ; with symbols at each leaf but no value at node
+    ; varient named Leaf and Node
+    ; each node has exactly two children
+    ; include define-type
+
+
+
+
+
+
+
+
+
+;; 2.6 ZZ-Tree
+    ; develop the zz-tree function that accepts a binary tree and produces a new binary tree
+    ; symbol 'zz
+
+
+
+
+
+
+
+
+
+;; 2.7 Mirror
+    ; develop the mirror function that accpets abinary tree and produces a new binary tree
+    ; left-right mirror image
+
+
+
+
+
+
+
+
+
+;; 2.8 Min-Depth
+    ; Develop the contains? function that accepts a binary tree and a symbol and return true
+
+
+
+
+
+
+
+
+
+
+;; 2.10 Subsitution
+    ; Develop the subst function that accpets a source BTree and a symbol and a replacemnet BTree
+
+
+
+
+
+
+
+
+
+
+;; 2.11 All Path Lengths
+    ; develop the all-path-lengths function that accepts a binary tree and returns a lsit containing the legnths of all of the paths
--- Lab2/lab2.rkt
+++ Lab2/lab2.rkt
@@ -1,2 +1,86 @@
-#lang racket
+#lang typed/racket
+(require typed/rackunit)
 
+;; List Template
+; (define (list-template [lst : (listof Any)]) : Any
+;     (match lst
+;         ['() ...]
+;         [(cons first rest) (... first ... (list-template rest) ...)]))
+
+;; 1. Develop the function rev-str-app
+(define (rev-str-app [l : (Listof String)]) : String
+    (match l
+        ['() ""]        ; <look for> <return>
+        [(cons f r) (string-append (rev-str-app r) f)]))
+
+;; Test Case
+(check-equal? (rev-str-app '("")) "")
+(check-equal? (rev-str-app '("ball" "juice" "frog")) "frogjuiceball")
+(check-equal? (rev-str-app '("ball" "" "frog")) "frogball")
+
+
+;; 2. Use :print-type
+; 1. What type does rev-str-app have?   the type is (-> (Listof String) String)
+; 2. Does this make sense?              Yes, it tell the rev-str-app is type Listof String
+; 3. What about the type of ’+’?        the type is a bunch of (case -> type
+; 4. Why is it so long?                 Because '+' operate many path of adding integer, reals, numbers, etc.
+
+
+;; 3. Develop representation for bicycles
+(struct Trek ([num-wheels : Integer]) #:transparent)
+(struct Bianchi ([num-wheels : Integer]) #:transparent)
+(struct Gunnar ([num-wheels : Integer]) #:transparent)
+
+; define the union type for bicyle 
+(define-type Bicycle (U Trek Bianchi Gunnar))
+
+; testing
+(define trek-bike (Trek 2))
+(define bianchi-bike (Bianchi 2))
+(define gunnar-bike (Gunnar 2))
+
+;; 4. develop the function only-treks (no filter)
+(define (only-treks [l : (Listof Bicycle)]) : (Listof Bicycle)
+    (match l
+        ['() '()]
+        [(cons f r)
+            (if (Trek? f)
+                (cons f (only-treks r))
+                (only-treks r))]))
+; Test case
+(check-equal? (only-treks '()) '())
+(check-equal? (only-treks (list trek-bike bianchi-bike gunnar-bike)) (list trek-bike))
+(check-equal? (only-treks (list trek-bike trek-bike gunnar-bike)) (list trek-bike trek-bike))
+(check-equal? (only-treks (list bianchi-bike gunnar-bike)) '())
+
+
+;; 5. develop the function only-bianchis
+(define (only-bianchis [l : (Listof Bicycle)]) : (Listof Bicycle)
+    (match l
+        ['() '()]
+        [(cons f r)
+            (if (Bianchi? f)
+                (cons f (only-bianchis r))
+                (only-bianchis r))]))
+; Test case
+(check-equal? (only-bianchis '()) '())
+(check-equal? (only-bianchis (list trek-bike bianchi-bike gunnar-bike)) (list bianchi-bike))
+(check-equal? (only-bianchis (list bianchi-bike bianchi-bike gunnar-bike)) (list bianchi-bike bianchi-bike))
+(check-equal? (only-bianchis (list trek-bike gunnar-bike)) '())
+
+;; 6. abstract over the two to obtain the function onlyThese
+(define (onlyThese [l : (Listof Bicycle)] [x : (Any -> Boolean)]) : (Listof Bicycle)
+    (match l
+        ['() '()]
+        [(cons f r)
+            (if (x f)
+            (cons f (onlyThese r x))
+            (onlyThese r x))]))
+
+
+
+
+
+
+
+;; 7. develope the function my-append that consumes two lists and returns second one to the first result
